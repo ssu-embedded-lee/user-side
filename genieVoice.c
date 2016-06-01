1a4c91b3e012a0b351fccb3cd355a35a59211940
justin-hwaseob-lee <hs6959@gmail.com>
2016-06-01 21:55:31 +0900

    genieVoice modified

--- genieVoice.c
+++ genieVoice.c
@@ -3,15 +3,7 @@
 
 int main()
 {
-	if(fork() == 0)
-	{
-		sleep(1);
-		system("chromium-browser https://localhost:7777 --use-fake-ui-for-media-stream --kiosk");
-	}
-	else
-	{
-		chdir("/var/www/html");
-		system("python simple-https-server.py");
-	}
+	chdir("/var/www/html");
+	system("python simple-https-server.py");
 	return 0;
 }
