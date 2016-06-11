490bbee5144447e0f6be3aa8eea2e3aedaecc6a2
justin-hwaseob-lee <hs6959@gmail.com>
2016-06-11 19:26:09 +0900

    user+kernel success ver.1

--- html/load.php
+++ html/load.php
@@ -1,47 +1,21 @@
 <?php
 	$data = system("sudo /var/www/html/dowork 2");
 	
-	$f = fopen("sudo /var/www/html/loadData.txt", "r");
-	
+	$f = fopen("/var/www/html/loadData.txt", "r");
+
+	ob_start();
+
 	while(!feof($f))
 	{
 		$command = fgets($f) . "<br/>";
 		
 		if($command == "학교불 켜")
 		{
-			$.ajax({
-                                        type:"GET",
-                                        async:"false",
-                                        url:"http://192.168.0.25/lighton.php",
-                                        dataType:"jsonp",
-                                        data:"list",
-                                        callback:"temper",
-                                        success:function(data){
-                                                responsiveVoice.speak("불을 켰습니다!", "Korean Female");
-                                        },
-                                        error:function(){
-                                         alert("Fail");
-                                         }
-                                });
-
+			file_get_contents("http://192.168.0.25/lighton.php");
 		}
 		else if($command == "학교불 꺼")
 		{
-			$.ajax({
-                                        type:"GET",
-                                        async:"false",
-                                        url:"http://192.168.0.25/lightoff.php",
-                                        dataType:"jsonp",
-                                        data:"list",
-                                        callback:"temper",
-                                        success:function(data){
-                                                responsiveVoice.speak("불을 껐습니다!", "Korean Female");
-                                        },
-                                        error:function(){
-                                         alert("Fail");
-                                         }
-                                });
-
+                        file_get_contents("http://192.168.0.25/lightoff.php");
 		}
 	}
 	fclose($f);
--- html/setting.php
+++ html/setting.php
@@ -1,4 +1,5 @@
 <?php
 	$data=shell_exec("sudo /var/www/html/dowork 4 학교불");
+	shell_exec("sudo /var/www/html/dowork 6");
 	echo $_GET['callback'].'('.json_encode($data).')';
 ?>
