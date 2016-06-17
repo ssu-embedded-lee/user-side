e8659c567b2413b5994c4041729f6f8535464567
justin-hwaseob-lee <hs6959@gmail.com>
2016-06-17 17:21:49 +0900

    final vaersion compledted

--- html/inputProcess.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	fprintf(stdout, "hello...I'm stdout\n");
-	fprintf(stderr, "I'm stderr\n");
-	return 0;
-}
--- html/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-ob_start();
-$data = file_get_contents("http://192.168.0.25/lightoff.php");
-echo $data;
-?>
